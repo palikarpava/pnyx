5175fb31cc7a20e3c880be8c588fab2cde011071
nastassiya.palikarpava <higrina@gmail.com>
2018-05-22 23:14:51 -0700

    Update icons

--- includes/admin/pages/about.php
+++ includes/admin/pages/about.php
@@ -6,62 +6,62 @@ endif; // Shhh
 $texts = array(
 
 	array(
-		'icon'       => 'fa-smile',
+		'icon'       => 'user-friendly-approach.png',
 		'heading'     => __( 'User Friendly Approach', TP_TD ),
 		'description' => __( 'Our intuitive interface will not slow you down. Create entire page dedicated to polls and make them part of your articles. Easy, fast, stunning. ', TP_TD ),
 	),
 	array(
-		'icon'       => 'fa-piggy-bank',
+		'icon'       => 'promote-sales.png',
 		'heading'     => __( 'Promote Sales', TP_TD ),
 		'description' => __( 'Do you need to display a customized offer and call for action?  With our customization settings, you can have it all.', TP_TD ),
 	),
 	array(
-		'icon'       => 'fa-gem',
+		'icon'       => 'generate-leads.png',
 		'heading'     => __( 'Generate Leads', TP_TD ),
 		'description' => __( 'Do you need new leads and boost your conversion?  Gather additional information from voters utilizing custom field types.', TP_TD ),
 	),
 	array(
-		'icon'       => 'fa-whmcs',
+		'icon'       => 'analyze.png',
 		'heading'     => __( 'Analyze, Optimize, Improve', TP_TD ),
 		'description' => __( 'Analyze the conversion rates with using dashboard and optimize to improve results.', TP_TD ),
 	),
 	array(
-		'icon'       =>  'fa-wrench',
+		'icon'       =>  'customization.png',
 		'heading'     => __( 'Customization', TP_TD ),
 		'description' => __( 'Use our white template to add your branding, with basic knowledge of CSS and HTML you can elevate the look to the perfection of your web look. Don’t know how? Contact us to get a quote for customization of your web.', TP_TD ),
 	),
 	array(
-		'icon'       => 'fa-mobile-alt',
+		'icon'       => 'mobile-first-approach.png',
 		'heading'     => __( 'Mobile-First Approach', TP_TD ),
-		'description' => __( 'Pnyx uses mobile-first approach, that means that your polls will look right  on a laptop, tablet or a smartphone.', TP_TD ),
+		'description' => __( 'Pnyx uses mobile-first approach, that means that your polls will look right on a laptop, tablet or a smartphone.', TP_TD ),
 	),
 	array(
-		'icon'       => 'fa-searchengin',
+		'icon'       => 'seo.png',
 		'heading'     => __( 'SEO Friendly', TP_TD ),
 		'description' => __( 'Thus, Pnyx’s polls search-engine friendly to elevate your website traffic and get visible on the world web arena.', TP_TD ),
 	),
 	array(
-		'icon'       => 'fa-stopwatch',
+		'icon'       => 'voting-limitations.png',
 		'heading'     => __( 'Voting limitations', TP_TD ),
 		'description' => __( 'Pnyx has multiple ways to define the poll style: poll date period or ongoing, reviewing results for registered or unregistered users, voting with verification or without.', TP_TD ),
 	),
 	array(
-		'icon'       => 'fa-paint-brush',
+		'icon'       => 'poll-visualization.png',
 		'heading'     => __( 'Poll Visualization', TP_TD ),
 		'description' => __( 'Dress up your polls  with Text, Image, Video or even HTML. Select graphs, world maps and countdown clocks for the representation of the results.', TP_TD ),
 	),
 	array(
-		'icon'       => 'fa-map-signs',
+		'icon'       => 'validation-process.png',
 		'heading'     => __( 'Validation Process', TP_TD ),
 		'description' => __( 'Poll Log will help you identify cheating attempts and protection features will ensure your polls has legitimate data to rely on.', TP_TD ),
 	),
 	array(
-		'icon'       => 'fa-random',
+		'icon'       => 'integration.png',
 		'heading'     => __( 'Integration', TP_TD ),
 		'description' => __( 'Pnyx embeds your polls using a short code, widget or even a direct link.', TP_TD ),
 	),
 	array(
-		'icon'       => 'fa-life-ring',
+		'icon'       => 'support.png',
 		'heading'     => __( 'Support', TP_TD ),
 		'description' => __( 'We listen. Don’t be shy, let us know how can we improve our product. ', TP_TD ),
 	),
@@ -80,14 +80,9 @@ $credits = array(
 		'position' => 'Develops TotalPoll plugin which is extended by Pnyx',
 	),
 	array(
-		'url'      => 'http://iconmonstr.com',
-		'name'     => 'Iconmonstr',
-		'position' => 'Icons',
-	),
-	array(
-		'url'      => 'https://pixelbuddha.net/ballicons2/',
-		'name'     => 'Ballicons',
-		'position' => 'Icons',
+		'url'      => 'http://chartjs.org/',
+		'name'     => 'Chart.JS',
+		'position' => 'Charts',
 	),
 	array(
 		'url'      => 'http://xdsoft.net/jqplugins/datetimepicker/',
@@ -97,6 +92,11 @@ $credits = array(
 	array(
 		'url'      => 'http://fontawesome.com/',
 		'name'     => 'Font Awesome',
+		'position' => 'Icons',
+	),
+	array(
+		'url'      => 'http://www.iconshock.com/',
+		'name'     => 'Juan Sarmiento',
 		'position' => 'Icons',
 	),
 );
@@ -122,7 +122,7 @@ $credits = array(
 			<?php endif; ?>
 			<div class="boxes-cell">
 				<div class="clearfix">
-					<div class="about-sec-icon"><i class="fas fa-2x fa-fw <?php echo esc_attr( $feature['icon'] ); ?>"></i></div>
+					<div class="about-sec-icon"><img src="<?=TP_URL . 'assets/images/'. $feature['icon']?>" /></div>
 
 					<h3><?php echo $feature['heading']; ?></h3>
 
@@ -157,9 +157,4 @@ $credits = array(
 			<?php endforeach; ?>
 		</div>
 	</div>
-
-	<div class="return-to-dashboard">
-		<a href="<?php admin_url( 'edit.php?post_type=poll' ) ?>"><?php _e( 'Go to Polls', TP_TD ); ?></a>
-	</div>
-
 </div>
